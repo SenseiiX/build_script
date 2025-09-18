3514eba0403dbd5ecfcb2d9a20d5114d6362df85
SENX | センクス <senseixdev@gmail.com>
2025-09-18 20:38:33 +0800

    Update rising.sh

--- rising.sh
+++ rising.sh
@@ -30,7 +30,7 @@ rm -rf "${dirs_to_remove[@]}"
 echo "=== Cloning device trees ==="
 git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b rising-t device/xiaomi/munch
 git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch vendor/xiaomi/munch
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b wip-next kernel/xiaomi/munch
 git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch hardware/xiaomi
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
