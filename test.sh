27bd36d9dd9a66bea3e634b8e14d670ce5ba9f63
SENX | センクス <senseixdev@gmail.com>
2025-09-27 11:26:27 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+
+rm -rf .repo/local_manifests
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+rm -rf prebuilts/clang/host/linux-x86
+
+echo "==> Syncing sources..."
+/opt/crave/resync.sh
+
+echo "==> Cleaning old outputs and device/vendor/hardware trees..."
+dirs_to_remove=(
+    out/target/product/munch
+    out/target/product/vanilla
+    out/target/product/gapps
+    out/target/product/core
+    device/xiaomi/munch
+    kernel/xiaomi/munch
+    vendor/xiaomi/munch
+    vendor/xiaomi/munch-firmware
+    hardware/xiaomi
+    hardware/dolby
+    vendor/xiaomi/miuicamera
+    packages/resources/devicesettings
+)
+rm -rf "${dirs_to_remove[@]}"
+
+echo "=== Cloning device trees ==="
+git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b test2 device/xiaomi/munch
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16-t vendor/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b test kernel/xiaomi/munch
+git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
+
+# VANILLA
+echo "=== Building VANILLA variant ==="
+. build/envsetup.sh
+riseup munch userdebug && \
+make installclean && \
+rise b
+mv out/target/product/munch out/target/product/vanilla
+
+# GAPPS
+cd device/xiaomi/munch
+rm lineage_munch.mk
+mv gapps.txt lineage_munch.mk
+cd ../../..
+
+echo "=== Building GAPPS variant ==="
+. build/envsetup.sh
+riseup munch userdebug && \
+make installclean && \
+rise b
+mv out/target/product/munch out/target/product/gapps
+
+# CORE
+cd device/xiaomi/munch
+rm lineage_munch.mk
+mv core.txt lineage_munch.mk
+cd ../../..
+
+echo "=== Building CORE variant ==="
+. build/envsetup.sh
+riseup munch userdebug && \
+make installclean && \
+rise b
+mv out/target/product/munch out/target/product/core
+
+echo "===== All builds completed successfully! ====="
