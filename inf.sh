14c090824e4d3db6fe1e236fe4f02a816d9b9924
SENX | センクス <senseixdev@gmail.com>
2025-10-13 20:53:53 +0800

    Update and rename lunaris.sh to inf.sh

--- inf.sh
+++ inf.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests
-repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 rm -rf prebuilts/clang/host/linux-x86
 
 echo "==> Syncing sources..."
@@ -10,9 +10,6 @@ echo "==> Syncing sources..."
 echo "==> Cleaning old outputs and device/vendor/hardware trees..."
 dirs_to_remove=(
     out/target/product/munch
-    out/target/product/vanilla
-    out/target/product/gapps
-    out/target/product/core
     device/xiaomi/munch
     kernel/xiaomi/munch
     vendor/xiaomi/munch
@@ -25,9 +22,9 @@ dirs_to_remove=(
 rm -rf "${dirs_to_remove[@]}"
 
 echo "=== Cloning device trees ==="
-git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b lunaris device/xiaomi/munch
-git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch vendor/xiaomi/munch
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/munch
+git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b inf device/xiaomi/munch
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b wip-next kernel/xiaomi/munch
 git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
@@ -37,21 +34,7 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 # GAPPS
 echo "=== Building GAPPS ==="
 . build/envsetup.sh
-lunch lineage_munch-bp2a-user && \
-make installclean && \
-m lunaris
-mv out/target/product/munch out/target/product/gapps
-
-# CORE
-cd device/xiaomi/munch
-rm lineage_munch.mk
-mv core.txt lineage_munch.mk
-cd ../../..
-
-echo "=== Building CORE variant ==="
-riseup munch user && \
-make installclean && \
-rise b
-mv out/target/product/munch out/target/product/core
+lunch lineage_munch-user
+m bacon
 
 echo "===== Builds completed successfully! ====="
