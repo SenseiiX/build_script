530d4bdf520d977d0af1144fc81f7787360c09fe
SENX | センクス <senseixdev@gmail.com>
2025-10-23 18:09:28 +0800

    Update

--- axion.sh
+++ axion.sh
@@ -5,7 +5,12 @@ repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 
 echo "==> Syncing sources..."
-/opt/crave/resync.sh
+if [ -f /opt/crave/resync.sh ]; then
+    /opt/crave/resync.sh
+else
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+
 
 echo "==> Cleaning old outputs and device/vendor/hardware trees..."
 dirs_to_remove=(
@@ -19,18 +24,16 @@ dirs_to_remove=(
     hardware/xiaomi
     hardware/dolby
     vendor/xiaomi/miuicamera
-    packages/resources/devicesettings
 )
 rm -rf "${dirs_to_remove[@]}"
 
 echo "=== Cloning device trees ==="
 git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b ax device/xiaomi/munch
 git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b wip-noksu kernel/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b ax kernel/xiaomi/munch
 git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
 git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
 git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
 
 echo "=== Starting GMS (Pico) build ==="
--- rising.sh
+++ rising.sh
@@ -5,7 +5,11 @@ repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 
 echo "==> Syncing sources..."
-/opt/crave/resync.sh
+if [ -f /opt/crave/resync.sh ]; then
+    /opt/crave/resync.sh
+else
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
 
 echo "==> Cleaning old outputs and device/vendor/hardware trees..."
 dirs_to_remove=(
@@ -15,24 +19,22 @@ dirs_to_remove=(
     out/target/product/core
     device/xiaomi/munch
     kernel/xiaomi/munch
-    #vendor/xiaomi/munch
-    #vendor/xiaomi/munch-firmware
-    #hardware/xiaomi
-    #hardware/dolby
-    #vendor/xiaomi/miuicamera
-    packages/resources/devicesettings
+    vendor/xiaomi/munch
+    vendor/xiaomi/munch-firmware
+    hardware/xiaomi
+    hardware/dolby
+    vendor/xiaomi/miuicamera
 )
 rm -rf "${dirs_to_remove[@]}"
 
 echo "=== Cloning device trees ==="
 git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b rise device/xiaomi/munch
-#git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b wip-next kernel/xiaomi/munch
-#git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
-#git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
-#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
-#git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b rise kernel/xiaomi/munch
+git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
 
 # VANILLA
 echo "=== Building VANILLA variant ==="
