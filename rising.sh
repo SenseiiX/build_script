7df2867e212d6438c895963dea13668970c7ccb9
SENX | センクス <senseixdev@gmail.com>
2025-10-31 18:10:47 +0800

    dirty build

--- rising.sh
+++ rising.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
-rm -rf prebuilts/clang/host/linux-x86
+#rm -rf prebuilts/clang/host/linux-x86
 
 echo "==> Syncing sources..."
 if [ -f /opt/crave/resync.sh ]; then
@@ -17,24 +17,24 @@ dirs_to_remove=(
     out/target/product/vanilla
     out/target/product/gapps
     out/target/product/core
-    device/xiaomi/munch
-    kernel/xiaomi/munch
-    vendor/xiaomi/munch
-    vendor/xiaomi/munch-firmware
-    hardware/xiaomi
-    hardware/dolby
-    vendor/xiaomi/miuicamera
+    #device/xiaomi/munch
+    #kernel/xiaomi/munch
+    #vendor/xiaomi/munch
+    #vendor/xiaomi/munch-firmware
+    #hardware/xiaomi
+    #hardware/dolby
+    #vendor/xiaomi/miuicamera
 )
 rm -rf "${dirs_to_remove[@]}"
 
 echo "=== Cloning device trees ==="
-git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b rise device/xiaomi/munch
-git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b rise kernel/xiaomi/munch
-git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
-git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
-git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
+#git clone https://github.com/Project-SenX/android_device_xiaomi_munch -b rise device/xiaomi/munch
+#git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
+#git clone https://github.com/SenseiiX/fusionX_sm8250 -b rise kernel/xiaomi/munch
+#git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
+#git clone https://github.com/Project-SenX/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+#git clone https://github.com/Project-SenX/android_vendor_xiaomi_miuicamera -b vic vendor/xiaomi/miuicamera
 
 # VANILLA
 echo "=== Building VANILLA variant ==="
