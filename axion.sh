9b2455a376ed99d0dc6d67c1c364f05b97e0dbef
SENX | センクス <senseixdev@gmail.com>
2026-04-03 19:08:41 +0800

    Fix local manifest URL and clean up build script
    Updated the local manifest repository URL and removed the vanilla build process.

--- axion.sh
+++ axion.sh
@@ -3,7 +3,7 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 
 # Clone local manifest
-git clone https://github.com/SenX-Project/local_manifest -b ax .repo/local_manifests
+git clone https://github.com/Project-SenX/local_manifest -b axion .repo/local_manifests
 
 echo "==> Syncing sources..."
 if [ -f /opt/crave/resync.sh ]; then
@@ -16,7 +16,6 @@ echo "==> Cleaning old outputs..."
 dirs_to_remove=(
     out/target/product/munch
     out/target/product/gapps
-    out/target/product/vanilla
 )
 rm -rf "${dirs_to_remove[@]}"
 
@@ -25,11 +24,5 @@ echo "=== Starting GMS (Pico) build ==="
 axion munch user gms pico
 ax -br
 mv out/target/product/munch out/target/product/gapps
-
-echo "=== Starting Vanilla (AOSP) build ==="
-. build/envsetup.sh
-axion munch user va
-ax -br
-mv out/target/product/munch out/target/product/vanilla
 
 echo "=== All builds completed successfully! ==="
