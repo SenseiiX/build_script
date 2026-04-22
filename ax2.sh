22eae5516cd2ed3f5c9e4ffe8888f7228e1183e2
SENX | センクス <senseixdev@gmail.com>
2026-04-23 06:41:12 +0800

    Add build script for GMS and Vanilla versions
    This script initializes the repo, syncs sources, cleans old outputs, and builds GMS and Vanilla versions.

---
+++ ax2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+rm -rf .repo/local_manifests
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+
+# Clone local manifest
+git clone https://github.com/SenX-Project/local_manifest -b ax2 .repo/local_manifests
+
+echo "==> Syncing sources..."
+if [ -f /opt/crave/resync.sh ]; then
+    /opt/crave/resync.sh
+else
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+
+echo "==> Cleaning old outputs..."
+dirs_to_remove=(
+    out/target/product/munch
+    out/target/product/gapps
+    out/target/product/vanilla
+)
+rm -rf "${dirs_to_remove[@]}"
+
+echo "=== Starting GMS build ==="
+. build/envsetup.sh
+axion munch user gms
+ax -br
+mv out/target/product/munch out/target/product/gapps
+
+echo "=== Starting Vanilla build ==="
+. build/envsetup.sh
+axion munch user va
+ax -br
+mv out/target/product/munch out/target/product/vanilla
